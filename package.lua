ab95fe8a4a83228031a0076d47d768734da2e93b
Tim Caswell <tim@creationix.com>
2015-04-14 22:30:54 -0500

    Specify tiny luvi

--- package.lua
+++ package.lua
@@ -2,6 +2,10 @@ return {
   name = "rainbows",
   version = "0.0.0",
   private = true,
+  luvi = {
+    flavor = "tiny",
+    version = "1.1.0"
+  },
   dependencies = {
     "luvit/require",
     "luvit/pretty-print",
